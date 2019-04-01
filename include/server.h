022d5ae6338798fcb9fedf7e1cab75bc73a34a99
Arthur Dassier <arthur.dassier@epitech.eu>
2019-04-01 17:25:41 +0200

    [STARTING PROJECT]

---
+++ include/server.h
@@ -0,0 +1,28 @@
+/*
+** EPITECH PROJECT, 2019
+** server.h
+** File description:
+** server
+*/
+
+#ifndef SERVER_H_
+    #define SERVER_H_
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/time.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#define INVALID_SOCKET -1
+#define SOCKET_ERROR -1
+#define PORT 1025
+
+typedef int SOCKET;
+typedef struct sockaddr_in SOCKADDR_IN;
+typedef struct sockaddr SOCKADDR;
+
+#endif /* !SERVER_H_ */
